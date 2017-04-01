2605df1f33856f54b8caa2441e3109fe1645640a
park <sttgpark2@naver.com>
2017-04-01 17:50:31 +0900

    첫번째 태수앱 커밋

--- app/src/main/java/first/taesu/calendarDetailDateAdd.java
+++ app/src/main/java/first/taesu/calendarDetailDateAdd.java
@@ -10,6 +10,8 @@ public class calendarDetailDateAdd extends AppCompatActivity {
 
     Button DateAdd_button;
 
+    Button ad;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
